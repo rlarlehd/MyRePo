e690fecdefc5cce3f3f49ec517b9b929770d6e1e
ykchu <ykchu@euclidsoft.co.kr>
2025-02-27 21:39:12 +0900

    4일

--- day4/tensor_cnn_cifar10.py
+++ day4/tensor_cnn_cifar10.py
@@ -1,21 +1,53 @@
 import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
-import matplotlib.pyplot as plt # 설치 → pip install matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 
-(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+# 1. CIFAR-10 데이터 로드 및 전처리
+(x_train, y_train), (x_test, y_test) = datasets.cifar10.load_data()
+
 # Normalize pixel values to be between 0 and 1
-train_images, test_images = train_images / 255.0, test_images / 255.0
-class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
-'dog', 'frog', 'horse', 'ship', 'truck']
+x_train, x_test = x_train / 255.0, x_test / 255.0
 
-plt.figure(figsize=(10,10))
-for i in range(25):
-    plt.subplot(5,5,i+1)
+# CIFAR-10 클래스 이름
+class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+               'dog', 'frog', 'horse', 'ship', 'truck']
+
+# 2. CNN 모델 정의
+model = models.Sequential([
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.Flatten(),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(10)  # Output layer with 10 classes
+])
+
+# 모델 요약 출력
+model.summary()
+
+# 3. 모델 컴파일 및 학습
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+history = model.fit(x_train, y_train, epochs=10,
+                    validation_data=(x_test, y_test))
+
+# 4. 테스트 데이터 예측 및 시각화
+test_samples = 10  # 시각화할 테스트 데이터 수
+predictions = model.predict(x_test[:test_samples])
+predicted_labels = np.argmax(predictions, axis=1)
+
+plt.figure(figsize=(15, 5))
+for i in range(test_samples):
+    plt.subplot(2, 5, i + 1)
     plt.xticks([])
     plt.yticks([])
     plt.grid(False)
-    plt.imshow(train_images[i])
-    # The CIFAR labels happen to be arrays,
-    # which is why you need the extra index
-    plt.xlabel(class_names[train_labels[i][0]])
-    plt.show()
+    plt.imshow(x_test[i], cmap=plt.cm.binary)
+    plt.xlabel(f"True: {class_names[y_test[i][0]]}\nPred: {class_names[predicted_labels[i]]}")
+plt.tight_layout()
+plt.show()
